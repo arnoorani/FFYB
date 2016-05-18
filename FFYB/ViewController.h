a3ad2c6c68778abfdbf1977a23b71b17cc3aed70
Suraj Naik <surajnaik@surajs-macbook-pro-2.local>
2016-05-18 14:03:50 +0530

    Initial Commit

---
+++ FFYB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FFYB
+//
+//  Created by Suraj Naik on 18/05/16.
+//  Copyright © 2016 Geniusu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FFYB/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FFYB
+//
+//  Created by Suraj Naik on 18/05/16.
+//  Copyright © 2016 Geniusu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
